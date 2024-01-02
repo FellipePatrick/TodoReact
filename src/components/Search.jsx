46a96deb2b3b60a2e5ce21e8862d000caaf2ebf8
fellipepatrick <fellipe.patrick@escolar.ifrn.edu.br>
2024-01-02 11:34:50 -0300

    Crud ToDo basico finalizado

---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+
+const Search = ({search, setSearch}) => {
+  return (
+    <div className="search">
+        <h2>Pesquisar</h2>
+        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Digite a tarefa..." />
+    </div>
+  )
+}
+
+export default Search
---
+++ src/todos.js
@@ -0,0 +1,14 @@
+[
+  {
+    "id": 1,
+    "text": "Criar funcionalidade",
+    "category": "Trabalho",
+    "isCompleted": false
+  },
+  {
+    "id": 2,
+    "text": "Criar funcionalidade 2",
+    "category": "Trabalho",
+    "isCompleted": false
+  }
+]
